6a5e29b0b7ebf0af18f56c540097b5f20e1cda7e
r3-da <haddan.reda.net@gmail.com>
2024-12-05 12:20:18 +0100

    add test REST Client request backend: - trigger sqlite seeder on backend start - upgrade to .net 8 frontend: - add debug config on launch.json

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,29 @@
 using API.Extensions;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
-
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddCors();
 
+// Register DataContext for dependency injection
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+// Ensure that the database is created and seed the data
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await Seed.SeedCities(context);  // Seed the cities
+}
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
